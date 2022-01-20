312496e8f410a8b7c5a8069a3aadf255d261be0a
Sergio Garnica <sgarnica1902@gmail.com>
2022-01-20 11:58:10 -0600

    mend

---
+++ src/EmptySearchTodos/index.js
@@ -0,0 +1,11 @@
+import "./EmptySearchTodos.css";
+
+function EmptySearchTodos({ searchValue }) {
+  return (
+    <section className="EmptyTodos">
+      <h2>There are no results for {searchValue}</h2>
+    </section>
+  );
+}
+
+export { EmptySearchTodos };
