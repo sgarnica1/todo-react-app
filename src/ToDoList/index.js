3b929943e4db22a5770968af37adefbf5f2b16b3
Sergio Garnica <sgarnica1902@gmail.com>
2022-01-20 12:01:25 -0600

    mend

--- src/ToDoList/index.js
+++ src/ToDoList/index.js
@@ -5,20 +5,28 @@ export function TodoList(props) {
   return (
     <section className="TodoList">
       <ul>
+        {/* Error */}
         {props.error && props.onError()}
+
+        {/* Loading */}
         {!props.error &&
           props.loading &&
           new Array(3).fill().map((item, index) => props.onLoading(index))}
+
+        {/* Empty todos list */}
         {!props.error &&
           !props.loading &&
-          props.totalTodos === 0 &&
-          !props.filteredTodos.length &&
+          !props.totalTodos &&
           props.onEmptyTodos()}
+
+        {/* No results */}
         {!props.error &&
           !props.loading &&
           props.totalTodos > 0 &&
           !props.filteredTodos.length &&
           props.onEmptySearchResults(props.searchValue)}
+
+        {/* Todos list */}
         {props.filteredTodos.map(props.render)}
 
         {props.children}
